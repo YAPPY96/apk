b86f9da41bdedab5bd16709980289d4bc109e585
YAPPY96 <yasufumi0906@gmail.com>
2025-08-16 13:35:51 +0900

    Second

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,12 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
-
+import Entypo from '@expo/vector-icons/Entypo';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
@@ -29,24 +28,18 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="map"
-        options={{
           title: 'Map',
-          tabBarIcon:({ color }) => <IconSymbol size={28} name ="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Entypo name="map" size={24} color="black" />,
         }}
       />
+      <Tabs.Screen
+        name="event"
+        options={{
+          title: 'event',
+          tabBarIcon: ({ color }) => <MaterialIcons name="event-note" size={24} color="black" />,
+        }}
+      />
+      
     </Tabs>
   );
 }
